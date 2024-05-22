b81c0904505aa434e0069b579d2d89eb012740d6
S0mbr4xc <s3._xc@hotmail.com>
2024-05-21 23:47:26 -0500

    LOGIN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { LoginComponent } from './paginas/login/login.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent, LoginComponent]
 })
 export class AppComponent {
   title = 'ProyectoBiblioteca';
