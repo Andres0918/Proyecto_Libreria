18c2cd24c92d42646aef02fed209fe63b1ee014e
Andres0918 <chablaandres@gmail.com>
2024-05-22 19:10:31 -0500

    Cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './paginas/login/login.component';
 import { InicioComponent } from './paginas/inicio/inicio.component';
+import { HistoriaComponent } from './paginas/historia/historia.component';
+import { BibliotecaComponent } from './paginas/biblioteca/biblioteca.component';
+import { GenerosComponent } from './paginas/generos/generos.component';
 
 
 export const routes: Routes = [
     { path:"login", component: LoginComponent },
-    { path:"inicio", component: InicioComponent }
+    { path:"inicio", component: InicioComponent },
+    { path:"historia", component: HistoriaComponent },
+    { path:"biblioteca", component: BibliotecaComponent},
 ];
