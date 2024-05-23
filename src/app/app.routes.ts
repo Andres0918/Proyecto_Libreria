bfb5e022705b2f34bc640b6ac5c561efbbaa04fe
S0mbr4xc <s3._xc@hotmail.com>
2024-05-22 19:12:55 -0500

    Actualizacion de version de firebase y login y registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './paginas/login/login.component';
 import { InicioComponent } from './paginas/inicio/inicio.component';
+import { RegistroComponent } from './paginas/registro/registro.component';
 
 
 export const routes: Routes = [
+    {path: "", redirectTo: "inicio", pathMatch: "full"},
     { path:"login", component: LoginComponent },
-    { path:"inicio", component: InicioComponent }
+    { path:"inicio", component: InicioComponent },
+    {path: "registro", component: RegistroComponent}
 ];
---
+++ src/app/domain/user.ts
@@ -0,0 +1,5 @@
+export class user{
+  nombre!: string
+  email!: string
+  password!: string
+}
