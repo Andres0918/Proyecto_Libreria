01fa4a9e81853bca09e5e6d68b2619ef782bad56
S0mbr4xc <s3._xc@hotmail.com>
2024-05-30 01:04:16 -0500

    a

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,7 +1,7 @@
 import { CanActivateFn } from '@angular/router';
 import { UserService } from './services/user.service';
 
-export const authGuard: CanActivateFn = (route, state) => 
+export const authGuard: CanActivateFn = (route, state) => {
   
   return true;
 };
