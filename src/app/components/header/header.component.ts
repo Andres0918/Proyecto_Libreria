384b654c21b4e545d2425867e7e1ca8f60faf2de
S0mbr4xc <s3._xc@hotmail.com>
2024-05-30 14:44:38 -0500

    a

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,7 +3,6 @@ import { Auth } from '@angular/fire/auth';
 import { RouterLink } from '@angular/router';
 import { onAuthStateChanged } from 'firebase/auth';
 import { UserService } from '../../services/user.service';
-import { of, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -26,27 +25,36 @@ export class HeaderComponent {
     const commonLink = document.getElementById('common')
 
     onAuthStateChanged(this.auth, async (user) => {
-      console.log('usuario: ',user)
-
-      this.userServices.getRoleByEmail(this.auth.currentUser?.email).subscribe(
-        role => {
-          console.log('Rol: ', role)
-
-          if(adminLink && commonLink){
-            if(role === 'admin'){
-              adminLink.style.display='block'
-              commonLink.style.display='none'
-            }else{
+      console.log('usuario xd: ',user)
+      if(this.auth.currentUser?.email){
+        this.userServices.getRoleByEmail(this.auth.currentUser.email).subscribe(
+          role => {
+            console.log('Rol: ', role)
+            if(adminLink && commonLink){
+              if(role === 'admin'){
+                adminLink.style.display='block'
+                commonLink.style.display='none'
+              }else{
+                adminLink.style.display='none'
+                commonLink.style.display='block'
+              }
+            }
+          },
+          error => {
+            console.error('Error fetching role', error);
+            this.role = null;
+            if(adminLink && commonLink){
               adminLink.style.display='none'
               commonLink.style.display='block'
             }
           }
-        },
-        error => {
-          console.error('Error fetching role', error);
-          this.role = null; // Manejo de error
+        );
+      }else{
+        if(adminLink && commonLink){
+          adminLink.style.display='none'
+          commonLink.style.display='block'
         }
-      );
+      }
       
       if(user){
         if(loggetInLink && loggetOutLink){
@@ -58,9 +66,9 @@ export class HeaderComponent {
           loggetOutLink.style.display = 'none'
           loggetInLink.style.display = 'block'
         }
-      }
-
-      
+      }      
+    }, error => {
+      console.log('Error en el auth: ', error)
     })
   }
 }
